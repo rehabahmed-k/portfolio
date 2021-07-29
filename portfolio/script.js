3795abb1f6ebe7cd8803f8d5aaf61353eca73e2a
Ayman <aymanooovprivate@gmail.com>
2021-07-29 10:36:03 +0300

    Ayman

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,1 +1,16 @@
 $('.carousel').carousel()
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
